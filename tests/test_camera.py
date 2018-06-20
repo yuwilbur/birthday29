34620a20285a05e648e954a1fdbb029149c19396
Wilbur Yu <yuwilbur@gmail.com>
2018-06-20 01:03:19 +0000

    Got a working prototype of rudimentary image processing working

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -1,30 +1,55 @@
-import io
+import numpy as np
 import picamera
 import pygame
+import pygame.camera
 import time
-import numpy
 
-WIDTH = 960
+WIDTH = 640
 HEIGHT = 480
+
+def y2rgb(y):
+    rgb = np.empty((WIDTH,HEIGHT,3),dtype=np.uint8)
+    rgb[:,:,2]=rgb[:,:,1]=rgb[:,:,0]=y
+    return rgb
+
+def testProcess(data):
+    total = 0
+    for i in range(0, data.size - 1):
+        total += data.item(i)
+    print("Test Process Total: %i" % total)
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 
 camera = picamera.PiCamera()
 camera.resolution = (WIDTH,HEIGHT)
-camera.framerate = 30
-camera.shutter_speed = 1000
-camera.crop = (0.0, 0.0, 1.0, 1.0)
+camera.shutter_speed = 1200
 
-x = (screen.get_width() - camera.resolution[0]) / 2
-y = (screen.get_height() - camera.resolution[1]) / 2
-
-data = numpy.empty((WIDTH, HEIGHT, 3), dtype=numpy.uint8);
+data = np.empty((WIDTH,HEIGHT,3),dtype=np.uint8)
 
 while(True):
-    start = time.time()
-    camera.capture(data, use_video_port=True, format='rgb')
-    screen.blit(pygame.image.frombuffer(data, (WIDTH, HEIGHT), 'RGB'), (x,y))
+    total_start = time.time()
+
+    camera_start = time.time()
+    data = np.empty((WIDTH,HEIGHT,3),dtype=np.uint8)
+    camera.capture(data, use_video_port=True, format='yuv')
+    data.resize((WIDTH,HEIGHT))
+    camera_end = time.time()
+
+    process_start = time.time()
+    testProcess(data)
+    process_end = time.time()
+
+    display_start = time.time()
+    surface = pygame.image.frombuffer(y2rgb(data), (WIDTH, HEIGHT), 'RGB')
+    screen.blit(surface, (0,0))
     pygame.display.update()
-    finish = time.time()
-    print("Time: %.2f" % (finish - start))
+    display_end = time.time()
+
+    total_end = time.time()
+    
+    total_time = total_end - total_start
+    camera_time = camera_end - camera_start
+    process_time = process_end - process_start
+    display_time = display_end - display_start
+    print("Total: %.3f, Camera: %.3f, Process: %.3f, Display: %.3f" % (total_time, camera_time, process_time, display_time))
