0a28b95951f59df5f49b439eafc34d72bb8dd360
Wilbur Yu <yuwilbur@gmail.com>
2018-06-20 01:49:34 +0000

    Converted capture to capture_continuous

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -25,16 +25,11 @@ camera = picamera.PiCamera()
 camera.resolution = (WIDTH,HEIGHT)
 camera.shutter_speed = 1200
 
+camera_start = time.time()
 data = np.empty((WIDTH,HEIGHT,3),dtype=np.uint8)
-
-while(True):
-    total_start = time.time()
-
-    camera_start = time.time()
-    data = np.empty((WIDTH,HEIGHT,3),dtype=np.uint8)
-    camera.capture(data, use_video_port=True, format='yuv')
-    data.resize((WIDTH,HEIGHT))
+for foo in camera.capture_continuous(data, use_video_port=True, format='yuv'):
     camera_end = time.time()
+    data.resize((WIDTH,HEIGHT), refcheck=False)
 
     process_start = time.time()
     testProcess(data)
@@ -46,10 +41,9 @@ while(True):
     pygame.display.update()
     display_end = time.time()
 
-    total_end = time.time()
-    
-    total_time = total_end - total_start
     camera_time = camera_end - camera_start
     process_time = process_end - process_start
     display_time = display_end - display_start
-    print("Total: %.3f, Camera: %.3f, Process: %.3f, Display: %.3f" % (total_time, camera_time, process_time, display_time))
+    print("Camera: %.3f, Process: %.3f, Display: %.3f" % (camera_time, process_time, display_time))
+    data.resize((WIDTH,HEIGHT,3), refcheck=False)
+    camera_start = time.time()
