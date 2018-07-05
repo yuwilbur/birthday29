7ae8f05f4588b14f6479d1728ed01ffb2604af48
Wilbur Yu <yuwilbur@gmail.com>
2018-07-05 18:04:01 +0000

    Added event dispatcher system

---
+++ common/event.py
@@ -0,0 +1,41 @@
+class Event(object):
+    def __init__(self, event_type, data=None):
+        self._type = event_type
+        self._data = data
+
+    def type(self):
+        return self._type
+
+    def data(self):
+        return self._data
+
+class EventDispatcher(object):
+    def __init__(self):
+        self._events = dict()
+
+    def __del__(self):
+        self._events = None
+
+    def has_listener(self, event_type, listener):
+        if event_type in self._events.keys():
+            return listener in self._events[event_type]
+        else:
+            return False
+
+    def add_event_listener(self, event_type, listener):
+        if not self.has_listener(event_type, listener):
+            if not event_type in self._events.keys():
+                self._events[event_type] = []
+            self._events[event_type].append(listener)
+
+    def remove_event_listener(self, event_type, listener):
+        if self.has_listener(event_type, listener):
+            self._events[event_type].remove(listener)
+            if len(self._events[event_type]) == 0:
+                del self._events[event_type]
+
+    def dispatch_event(self, event):
+        if event.type() in self._events.keys():
+            for listener in self._events[event.type()]:
+                listener(event)
+
