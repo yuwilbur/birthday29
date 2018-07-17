1ec27e0bf3fa78238acf939ea9d7737bd5ce1a30
Wilbur Yu <yuwilbur@google.com>
2018-07-17 13:26:11 -0700

    Moved physics to individual parts

--- core/engine/game_engine.py
+++ core/engine/game_engine.py
@@ -27,76 +27,51 @@ class GameEngine(Manager):
 		solid.getComponent(Solid).velocity += solid.getComponent(Solid).acceleration * PeriodSync.PERIOD
 		solid.position += solid.getComponent(Solid).velocity * PeriodSync.PERIOD
 
-	def isColliding(self, collider, reference):
-		def isCircleCircleColliding(circle1, circle2):
-			minDistanceSqu = math.pow(circle1.getComponent(Circle).radius + circle2.getComponent(Circle).radius, 2)
-			distanceSqu = Vector.DistanceSqu(circle1.position, circle2.position)
-			return distanceSqu <= minDistanceSqu
+	def runCircleCircleCollision(self, collider, reference):
+		min_distance_squ = math.pow(collider.getComponent(Circle).radius + reference.getComponent(Circle).radius, 2)
+		distance_squ = Vector.DistanceSqu(collider.position, reference.position)
+		if (distance_squ > min_distance_squ):
+			return
+		x1 = collider.position
+		m1 = collider.getComponent(Solid).mass
+		v1 = collider.getComponent(Solid).velocity
+		x2 = reference.position
+		m2 = reference.getComponent(Solid).mass
+		v2 = reference.getComponent(Solid).velocity
+		velocity = v1 - (x1 - x2) * (2 * m2 / (m1 + m1) * Vector.Dot(v1 - v2, x1 - x2) / Vector.DistanceSqu(x2, x1))
+		collider.getComponent(Solid).velocity = velocity
 
-		def isCircleRectangleColliding(circle, rectangle):
-			circle_comp = circle.getComponent(Circle)
-			rectangle_comp = rectangle.getComponent(Rectangle)
-			rectangle_topright = rectangle.position + (rectangle_comp.dimensions / 2)
-			rectangle_botleft = rectangle.position - (rectangle_comp.dimensions / 2)
-			if circle.position.x <= rectangle_topright.x and circle.position.x >= rectangle_botleft.x:
-				return math.pow(circle.position.y - rectangle.position.y, 2) < math.pow(circle_comp.radius + rectangle_comp.dimensions.y / 2, 2)
-			elif circle.position.y <= rectangle_topright.y and circle.position.y >= rectangle_botleft.y:
-				return math.pow(circle.position.x - rectangle.position.x, 2) < math.pow(circle_comp.radius + rectangle_comp.dimensions.x / 2, 2)
-			else:
-				circle_radius_squ = math.pow(circle.getComponent(Circle).radius, 2)
-				return (Vector.DistanceSqu(circle.position, Vector(rectangle_botleft.x, rectangle_botleft.y)) <= circle_radius_squ or
-					Vector.DistanceSqu(circle.position, Vector(rectangle_botleft.x, rectangle_topright.y)) <= circle_radius_squ or
-					Vector.DistanceSqu(circle.position, Vector(rectangle_topright.x, rectangle_botleft.y)) <= circle_radius_squ or
-					Vector.DistanceSqu(circle.position,Vector(rectangle_topright.x, rectangle_topright.y)) <= circle_radius_squ)
-
-		if collider.hasComponent(Circle):
-			if reference.hasComponent(Circle):
-				return isCircleCircleColliding(collider, reference)
-			if reference.hasComponent(Rectangle):
-				return isCircleRectangleColliding(collider, reference)
-		if collider.hasComponent(Rectangle):
-			if reference.hasComponent(Rectangle):
-				return False
-			if reference.hasComponent(Circle):
-				return isCircleRectangleColliding(reference, collider)
-		return False
-
-	def runCollision(self, collider, reference):
-		def runCircleCircleCollision(circle1, circle2):
-			x1 = circle1.position
-			m1 = circle1.getComponent(Solid).mass
-			v1 = circle1.getComponent(Solid).velocity
-			x2 = circle2.position
-			m2 = circle2.getComponent(Solid).mass
-			v2 = circle2.getComponent(Solid).velocity
-			velocity = v1 - (x1 - x2) * (2 * m2 / (m1 + m1) * Vector.Dot(v1 - v2, x1 - x2) / Vector.DistanceSqu(x2, x1))
-			collider.getComponent(Solid).velocity = velocity
-
-		def runCircleRectangleCollision(circle, rectangle):
-			x1 = circle.position
-			m1 = circle.getComponent(Solid).mass
-			v1 = circle.getComponent(Solid).velocity
-			x2 = rectangle.position
-			m2 = rectangle.getComponent(Solid).mass
-			v2 = rectangle.getComponent(Solid).velocity
-			d2 = rectangle.getComponent(Rectangle).dimensions
-			if (x1.x >= x2.x - d2.x / 2 and x1.x <= x2.x + d2.x / 2):
-				circle.getComponent(Solid).velocity = Vector(v1.x, -v1.y)
-			elif (x1.y >= x2.y - d2.y / 2 and x1.y <= x2.y + d2.y / 2):
-				circle.getComponent(Solid).velocity = Vector(-v1.x, v1.y)
-			else:
-				circle.getComponent(Solid).velocity = -Vector(v1.y, v1.x)
-
-		if collider.hasComponent(Circle):
-			if reference.hasComponent(Circle):
-				runCircleCircleCollision(collider, reference)
-			elif reference.hasComponent(Rectangle):
-				runCircleRectangleCollision(collider, reference)
-		if collider.hasComponent(Rectangle):
-			if reference.hasComponent(Rectangle):
+	def runCircleRectangleCollision(self, collider, reference):
+		circle = collider.getComponent(Circle)
+		rectangle = reference.getComponent(Rectangle)
+		rectangle_topright = reference.position + (rectangle.dimensions / 2)
+		rectangle_botleft = reference.position - (rectangle.dimensions / 2)
+		if collider.position.x <= rectangle_topright.x and collider.position.x >= rectangle_botleft.x:
+			if not (math.fabs(collider.position.y - reference.position.y) < circle.radius + rectangle.dimensions.y / 2):
 				return
-			if reference.hasComponent(Circle):
+		elif collider.position.y <= rectangle_topright.y and collider.position.y >= rectangle_botleft.y:
+			if not (math.fabs(collider.position.x - reference.position.x) < circle.radius + rectangle.dimensions.x / 2):
 				return
+		else:
+			circle_radius_squ = math.pow(circle.radius, 2)
+			if not (Vector.DistanceSqu(collider.position, Vector(rectangle_botleft.x, rectangle_botleft.y)) <= circle_radius_squ or
+				Vector.DistanceSqu(collider.position, Vector(rectangle_botleft.x, rectangle_topright.y)) <= circle_radius_squ or
+				Vector.DistanceSqu(collider.position, Vector(rectangle_topright.x, rectangle_botleft.y)) <= circle_radius_squ or
+				Vector.DistanceSqu(collider.position,Vector(rectangle_topright.x, rectangle_topright.y)) <= circle_radius_squ):
+				return
+		x1 = collider.position
+		m1 = collider.getComponent(Solid).mass
+		v1 = collider.getComponent(Solid).velocity
+		x2 = reference.position
+		m2 = reference.getComponent(Solid).mass
+		v2 = reference.getComponent(Solid).velocity
+		d2 = reference.getComponent(Rectangle).dimensions
+		if (x1.x >= x2.x - d2.x / 2 and x1.x <= x2.x + d2.x / 2):
+			collider.getComponent(Solid).velocity = Vector(v1.x, -v1.y)
+		elif (x1.y >= x2.y - d2.y / 2 and x1.y <= x2.y + d2.y / 2):
+			collider.getComponent(Solid).velocity = Vector(-v1.x, v1.y)
+		else:
+			collider.getComponent(Solid).velocity = -Vector(v1.y, v1.x)
 
 	def getSolids(self):
 		return self._solid_objects
@@ -130,9 +105,14 @@ class GameEngine(Manager):
 		for key in self._solid_objects:
 				self.runPhysics(self._solid_objects[key])
 		reference_objects = copy.deepcopy(self._collider_objects)
-		for collider in self._collider_objects:
-			for reference in reference_objects:
-				if collider == reference:
+		for (collider_key, collider) in self._collider_objects.items():
+			for (reference_key, reference) in reference_objects.items():
+				if collider_key == reference_key:
 					continue
-				if self.isColliding(self._collider_objects[collider], reference_objects[reference]):
-					self.runCollision(self._collider_objects[collider], reference_objects[reference])
+				if collider.hasComponent(Circle):
+					if reference.hasComponent(Circle):
+						self.runCircleCircleCollision(collider, reference)
+					if reference.hasComponent(Rectangle):
+						self.runCircleRectangleCollision(collider, reference)
+				#if self.isColliding(collider, reference):
+				#	self.runCollision(collider, reference)
