ee04446a1f6c3979c0d591fe46c30de8844d36dd
Wilbur Yu <yuwilbur@google.com>
2018-07-21 07:53:16 -0700

    Added an additional dash for config

--- yu29.py
+++ yu29.py
@@ -6,8 +6,12 @@ from core.main import Main
 if __name__ == '__main__':
 	main = Main()
 	for arg in sys.argv[1:]:
-		if arg[1:-2] == 'full_screen':
-			main.setFullScreen(arg[-1:] == '1')
+		if len(arg) < 5:
+			continue
+		arg_type = arg[2:-2]
+		arg_value = arg[-1:]
+		if arg_type == 'full_screen':
+			main.setFullScreen(arg_value == '1')
 	main.run()
 	pygame.quit()
 	sys.exit()
