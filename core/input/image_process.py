f8784fb2ce0106833407f6f9ba08c11df437f44e
Wilbur Yu <yuwilbur@google.com>
2018-07-21 20:13:11 -0700

    Added switch mechanism to make the players fair

--- core/input/image_process.py
+++ core/input/image_process.py
@@ -43,11 +43,20 @@ class ImageProcess(object):
         self._worker2.daemon = True
         self._worker2.start()
 
+        self._switch = True
+
     def processYImageEvent(self, event):
-        if not self._worker1_conn.poll():
-            self._main1_conn.send(event.data()[0])
-        if not self._worker2_conn.poll():
-            self._main2_conn.send(event.data()[1])
+        if self._switch:
+            if not self._worker1_conn.poll():
+                self._main1_conn.send(event.data()[0])
+            if not self._worker2_conn.poll():
+                self._main2_conn.send(event.data()[1])
+        else:
+            if not self._worker2_conn.poll():
+                self._main2_conn.send(event.data()[1])
+            if not self._worker1_conn.poll():
+                self._main1_conn.send(event.data()[0])
+        self._switch = not self._switch
 
     def stop(self):
         self._main1_conn.send(ImageProcess.END_MESSAGE)
