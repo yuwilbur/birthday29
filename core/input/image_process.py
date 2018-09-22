ce6e08b6af85c2ca30ae08af560da683d333f48e
Wilbur Yu <yuwilbur@google.com>
2018-09-22 16:27:38 -0700

    Added square image detection

--- core/input/image_process.py
+++ core/input/image_process.py
@@ -11,26 +11,29 @@ import numpy as np
 
 def processYImage(img):
     results = list()
-    def clearArea(top_left, bot_right):
-        img[top_left[0]:bot_right[0],top_left[1]:bot_right[1]] = 0
-        # for y in range(top_left[0], bot_right[0]):
-        #     for x in range(top_left[1], bot_right[1]):
-        #         results.append(ImageInput(np.array([y, x]), 0))
-    def getValue(top_left, bot_right):
-        count = (bot_right[0] - top_left[0] + 1) * (bot_right[1] - top_left[1] + 1)
-        if count == 0:
-            return 0.0
-        value = 0.0
-        for y in range(top_left[0], bot_right[0] + 1):
-            for x in range(top_left[1], bot_right[1] + 1):
-                value += img[y][x][0]
-        return value / count
     img_height = img.shape[0]
     img_width = img.shape[1]
     threshold = 125
     half_length = 8
     full_length = half_length * 2
-    rise = half_length / 2.0
+    rise = 2
+
+    def addPixel(y, x, direction):
+        results.append(ImageInput(np.array([y, x]), direction))
+    def clearArea(top_left, bot_right):
+        img[top_left[0]:bot_right[0],top_left[1]:bot_right[1]] = 0
+        # for y in range(top_left[0], bot_right[0]):
+        #     for x in range(top_left[1], bot_right[1]):
+        #         addPixel(y, x, 0)
+    def getValue(top_left, bot_right):
+        count = (bot_right[0] - top_left[0] + 1) * (bot_right[1] - top_left[1] + 1)
+        if count == 0:
+            return 0.0
+        value = 0.0
+        for y in range(max(top_left[0], 0), min(bot_right[0], img_height) + 1):
+            for x in range(max(top_left[1], 0), min(bot_right[1], img_width) + 1):
+                value += img[y][x][0]
+        return value / count
 
     img[0][0] = 0
     while True:
@@ -41,64 +44,51 @@ def processYImage(img):
         cy = candidate[0]
         cx = candidate[1]
         img[cy][cx] = 0
-        if min(cx, cy) <= half_length or max(cx, cy) >= img_height - half_length:
-            continue
-        if img[cy + rise][cx] < threshold:
-            continue
+        # Stop processing if the newest value is at the bottom.
+        if (cy > img_height - half_length):
+            break
 
-        left_slope = 0
-        x = cx
-        for y in range(cy + 1, cy + int(rise), +1):
-            for x in range(x, cx + half_length, +1):
-                if (img[y][x] < threshold):
-                    break
-        left_slope = (x - cx - 1) / rise
-        if left_slope == 0:
-            clearArea([cy, cx - half_length],[cy + full_length, cx + half_length])
+        y_limit = cy + rise
+        for y in range(cy + 1, cy + rise + 1, +1):
+            if (img[y][cx] < threshold):
+                break
+        if not (y == y_limit):
             continue
 
-        right_slope = 0
-        x = cx
-        for y in range(cy + 1, cy + int(rise), +1):
-            for x in range(x, cx - half_length, -1):
-                if (img[y][x] < threshold):
-                    break
-        right_slope = (x - cx - 1) / rise
-        if right_slope == 0:
-            clearArea([cy, cx - half_length],[cy + full_length, cx + half_length])
+        x_limit = min(cx + full_length, img_width)
+        for x in range(cx, x_limit + 1, +1):
+            if (img[y][x] < threshold):
+                #addPixel(y, x, 0)
+                break
+        x_right = x
+
+        x_limit = max(cx - full_length, 0)
+        for x in range(cx, x_limit - 1, -1):
+            if (img[y][x] < threshold):
+                #addPixel(y, x, 0)
+                break
+        x_left = x
+
+        x_center = (x_left + x_right) / 2
+
+        x_diff = x_center - cx
+        x_threshold = half_length / 4
+        length = int((x_right - x_left) * 1.2 / 2.0)
+
+        #addPixel(y, x_center, 0)
+
+        if (length < half_length):
+            clearArea([cy, x_center - length],[cy + length * 2, x_center + length])
             continue
 
-        mid_y = cy
-        bot_y = cy
-        slope = (right_slope + left_slope) / 2.0
-        for y in range(cy + 1, cy + full_length, +1):
-            x = cx + int(slope * (y - cy))
-            if (mid_y == cy):
-                if (img[y][x] < threshold):
-                    mid_y = y - 1
-            else:
-                if (img[y][x] >= threshold):
-                    bot_y = y - 1
-                    break
-        if bot_y == cy:
-            clearArea([cy, cx - half_length],[cy + full_length, cx + half_length])
-            continue
-
-        diff_y = (bot_y - mid_y) - (mid_y - cy)
-        if (diff_y * diff_y) > 2*2:
-            y = mid_y
-        else:
-            y = bot_y
-        
-        length = int((y - cy) * 1.25)
-        cy = y
-        cx = x
-
-        top = getValue([cy - 2, cx - 1], [cy - 2, cx + 1])
-        bottom = getValue([cy + 2, cx - 1], [cy + 2, cx + 1])
-        left = getValue([cy - 1, cx - 2], [cy + 1, cx - 2])
-        right = getValue([cy - 1, cx + 2], [cy + 1, cx + 2])
+        y = (x_right - x_left) / 2 + cy
+        x = x_center
+        top = getValue([y - 2, x - 1], [y - 2, x + 1])
+        bottom = getValue([y + 2, x - 1], [y + 2, x + 1])
+        left = getValue([y - 1, x - 2], [y + 1, x - 2])
+        right = getValue([y - 1, x + 2], [y + 1, x + 2])
         max_value = max(top, bottom, left, right)
+        key_direction = None
         if (top == max_value):
             key_direction = Key.DOWN
         elif (bottom == max_value):
@@ -108,10 +98,10 @@ def processYImage(img):
         elif (right == max_value):
             key_direction = Key.LEFT
         else:
-            break
-
-        results.append(ImageInput(np.array([cy, cx]), key_direction))
-        clearArea([cy - length, cx - length],[cy + length, cx + length])
+            continue
+        if not (key_direction == None):
+            addPixel(y, x, key_direction)
+        clearArea([cy, x_center - length],[cy + length * 2, x_center + length])
     return results
 
 def yImageWorker(pipe):
